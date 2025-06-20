af3286dcef7eaff550ca6a7cc125ed9f91f2a3ed
jakkakarthik04 <jsunnybabu@gmail.com>
2025-06-20 18:30:51 +0530

    updated api key

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyArvrNC44WT6BkiI76oOyYVyf68WMTLI0o",
+    apiKey: "aIzaSyArvrNC44WT6BkiI76oOyYVyf68WMTLI0o",
     authDomain: "otp-login-b825f.firebaseapp.com",
     projectId: "otp-login-b825f",
     storageBucket: "otp-login-b825f.appspot.com",
@@ -10,7 +10,7 @@ const firebaseConfig = {
     appId: "1:553965541688:web:ac8b3c1306df72744949cd"
   };
 
-  
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
